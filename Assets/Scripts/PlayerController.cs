79d1d6843a92258e0a863fbdb3128fc489fff493
Diarmid Campbell <dac70@cam.ac.uk>
2015-06-15 16:15:15 +0100

    Walking and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float walkSpeed = 10;
+	public BoxCollider2D standingBox;
+	public BoxCollider2D jumpingBox;
+	BoxCollider2D activeBox;
+	
 	GameObject model;
+	bool isGrounded = false;
+	bool tryJump = false;
 	
 	float horizontalSpeed;
 
@@ -17,8 +23,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		horizontalSpeed = walkSpeed * Input.GetAxis("Horizontal");
-		float absHSpeed = Mathf.Abs (horizontalSpeed);
-		if (absHSpeed > 0.01f){
+		if (Mathf.Abs (horizontalSpeed) > 0.01f){
 			if (horizontalSpeed > 0){
 				model.transform.rotation = Quaternion.Euler(0, 90, 0);
 			}
@@ -26,15 +31,64 @@ public class PlayerController : MonoBehaviour {
 				model.transform.rotation = Quaternion.Euler(0, -90, 0);
 			}
 		}
-		model.GetComponent<Animator>().SetFloat("speed", absHSpeed);
+		
+		tryJump = Input.GetMouseButton(0);
 		
 	}
 	
 	void FixedUpdate () {
 		//transform.position = transform.position + horizontalVel * Time.fixedDeltaTime;
+		
+		activeBox = isGrounded ? standingBox : jumpingBox;
+		Vector3 footPos = transform.position + new Vector3(0, activeBox.offset.y, 0) - 0.5f * new Vector3(0, activeBox.size.y, 0);
+		
+		
+		bool lineTestFwd = false;
+		if (Mathf.Abs(horizontalSpeed) > 0.1f){
+			Vector3 testVec = (new Vector3(horizontalSpeed, 0, 0)).normalized * (0.1f + activeBox.size.x * 0.5f);
+			Vector3 shinAdd = new Vector3(0, 0.01f, 0);
+			lineTestFwd = Physics2D.Linecast(footPos + shinAdd, footPos + shinAdd + testVec, 1 << LayerMask.NameToLayer("Ground"));
+			Debug.DrawLine(footPos + shinAdd, footPos + shinAdd + testVec, Color.yellow);
+			
+		}
+		if (lineTestFwd){
+			horizontalSpeed = 0f;
+		}
 		Vector3 newVel = GetComponent<Rigidbody2D>().velocity;
 		newVel.x = horizontalSpeed;
 		GetComponent<Rigidbody2D>().velocity = newVel;
+		model.GetComponent<Animator>().SetFloat("speed", Mathf.Abs (horizontalSpeed));
+		
+		
+		
+
+		//Debug.DrawLine(transform.position, footPos, Color.yellow);
+		
+		Vector3 startPos = footPos + new Vector3(0, 1f, 0);
+		Vector3 endPos = footPos + new Vector3(0, -0.1f, 0);
+		
+		
+		Vector3 widthVec = new Vector3(0.2f, 0, 0);
+		
+		bool lineTest1 = Physics2D.Linecast(startPos - widthVec, endPos - widthVec, 1 << LayerMask.NameToLayer("Ground"));
+		bool lineTest2 = Physics2D.Linecast(startPos + widthVec, endPos + widthVec, 1 << LayerMask.NameToLayer("Ground"));
+		
+		Debug.DrawLine(startPos - widthVec, endPos - widthVec, Color.red);
+		Debug.DrawLine(startPos + widthVec, endPos + widthVec, Color.red);
+		
+		isGrounded = lineTest1 || lineTest2;
+		model.GetComponent<Animator>().SetBool("isGrounded", isGrounded);
+		
+		if (tryJump && isGrounded){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1.5f), ForceMode2D.Impulse);
+		}
+		
+		// Set the collision box to the correct size
+		standingBox.enabled = isGrounded;
+		jumpingBox.enabled = !isGrounded;
+		
+
+			
 		
 	}
 }
